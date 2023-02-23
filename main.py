6ab2e05695d8e8c3daed377c03173f0a7413ee0e
Mevan Solanga <118385727+Mevan-Solanga@users.noreply.github.com>
2023-02-23 17:59:48 -0500

    Global Variables Added

--- main.py
+++ main.py
@@ -13,5 +13,36 @@ Hints:
 # Import whatever libraries/modules you need
 
 import numpy as np
+import sys
+import torch
+from torchvision.datasets import CIFAR10
+from torch.utils.data import DataLoader
+from torchvision.transforms import transforms
+import torch.nn as nn
+import torchvision
+import torch.nn.functional as F
+from torch.optim import Adam
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+
+# Global Constants
+BatchSize=5
+EPOCHS=10
+classes = ('plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Your working code here
+
+print("Batch Size: ", BatchSize)
+print("EPOCHS: ", EPOCHS)
+print("Device used: ", device)
+
+# Python version check (Had to install a lower version of python since pytorch didn't work on python 3.11)
+#print(f"Python Version: {sys.version}")
+
+# # Normalize the CIFAR-10 data 
+# transformations = transforms.Compose([
+#     transforms.ToTensor(),
+#     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+# ])
+
